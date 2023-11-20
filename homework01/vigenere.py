dd766bd536c9f2a8ec1d5b58e7e85c72a2e4a122
2004Ksenia <todorova_k@bk.ru>
2023-11-20 03:11:32 +0300

    Добавлено решение

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,42 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    a = 0
+    s = 0
+    b = 0
+    ciphertext = ''
+    while len(plaintext) > len(keyword):
+        keyword += keyword[a]
+        a += 1
+
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            if plaintext[i].isupper():
+                shift = ord(keyword[i].upper()) - ord('A')
+                if ord(plaintext[i]) + shift > ord("Z"):
+                    s = ord("Z") - ord(plaintext[i])
+                    shift2 = shift - s
+                    b = ord("A") + shift2 - 1
+                    a = chr(b)
+                    ciphertext += a
+                else:
+                    a = ord(plaintext[i]) + shift
+                    a = chr(a)
+                    ciphertext += a
+            if plaintext[i].islower():
+                shift = ord(keyword[i].lower()) - ord('a')
+                if ord(plaintext[i]) + shift > ord("z"):
+                    s = ord("z") - ord(plaintext[i])
+                    shift2 = shift - s
+                    b = ord("a") + shift2 - 1
+                    a = chr(b)
+                    ciphertext += a
+                else:
+                    a = ord(plaintext[i]) + shift
+                    a = chr(a)
+                    ciphertext += a
+        else:
+            ciphertext += str(plaintext[i])
     return ciphertext
 
 
@@ -24,5 +59,40 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    a = 0
+    s = 0
+    b = 0
+    plaintext = ''
+    while len(ciphertext) > len(keyword):
+        keyword += keyword[a]
+        a += 1
+
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            if ciphertext[i].isupper():
+                shift = ord(keyword[i].upper()) - ord('A')
+                if ord(ciphertext[i]) - shift < ord("A"):
+                    s = ord(ciphertext[i]) - ord("A")
+                    shift2 = shift - s
+                    b = ord("Z") - shift2 + 1
+                    a = chr(b)
+                    plaintext += a
+                else:
+                    a = ord(ciphertext[i]) - shift
+                    a = chr(a)
+                    plaintext += a
+            if ciphertext[i].islower():
+                shift = ord(keyword[i].lower()) - ord('a')
+                if ord(ciphertext[i]) - shift < ord("a"):
+                    s = ord(ciphertext[i]) - ord("a")
+                    shift2 = shift - s
+                    b = ord("z") - shift2 + 1
+                    a = chr(b)
+                    plaintext += a
+                else:
+                    a = ord(ciphertext[i]) - shift
+                    a = chr(a)
+                    plaintext += a
+        else:
+            plaintext += str(ciphertext[i])
     return plaintext
